8a967f7d0f4a69ccc33b90625fca40e7b15d7707
ygtysr <y.yasar92@gmail.com>
2018-08-07 10:51:10 +0300

    -- Interface designing.

--- binaryTree/binaryTree.h
+++ binaryTree/binaryTree.h
@@ -9,5 +9,25 @@
 #define BINARYTREE_H_
 
 
+typedef int TREE_SIZETYPE;
+typedef int TREE_DATATYPE;
+
+typedef struct _tagNode {
+
+	 struct _tagNode *pLeft;
+	 struct _tagNode *pRight
+	 TREE_DATATYPE   data;
+}treeNode_t;
+
+typedef struct _binaryTree {
+
+	treeNode_t 		rootNode;
+	TREE_SIZETYPE	size;
+}binartTree_t;
+
+
+treeNode_t createBinaryTree(void);
+treeNode_t insertItem(binartTree_t *pHandle, TREE_DATATYPE val);
+treeNode_t findItem(binartTree_t *pHandle, TREE_DATATYPE val);
 
 #endif /* BINARYTREE_H_ */
